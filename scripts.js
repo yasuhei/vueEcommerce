baa9b4ff84aa9b3bf9126439b0cd05e89345bb67
Yasuhei Nakamura <67479901+yasuhei@users.noreply.github.com>
2023-03-21 19:24:49 -0300

    somando os valores do carrinho

--- scripts.js
+++ scripts.js
@@ -69,7 +69,20 @@ const SelfServiceMachine = {
         return {
             products: window.products
         }
-    }
+    },
+    methods: {
+            total: function() {
+                var total = 0;
+
+                this.products.forEach(function(item) {
+                    if(item.active) {
+                        total += (item.price * item.quantity)
+                    }
+                });
+
+                return total.toFixed(2)
+            }
+    },
 };
 
 Vue.createApp(SelfServiceMachine).mount('#app')
